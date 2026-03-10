49c4a1c00d12854cf175c1d0ddf040c4b5f6e343
asavaripandit19 <asavaripandit2020@gmail.com>
2026-03-10 14:19:32 +0530

    add cross-orgin

--- Core-Banking-Application/src/main/java/com/bank/controllers/EmployeeController.java
+++ Core-Banking-Application/src/main/java/com/bank/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.bank.service.UserService;
 
 @RestController
 @RequestMapping("/accounts")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EmployeeController {
 
 	@Autowired
--- Core-Banking-Application/src/main/java/com/bank/controllers/UserController.java
+++ Core-Banking-Application/src/main/java/com/bank/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.bank.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.bank.service.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
 	@Autowired
